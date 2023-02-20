84719940549cde8f31a3f59a263ef27a5417cb21
SeriakovaOksana <hikki0019@gmail.com>
2023-02-20 12:21:47 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.num = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.num;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.num;
+        this.guess();
     }
 }
 
